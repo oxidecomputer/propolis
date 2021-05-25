c1563f5a6c31420d4ec386d41b7f9ac9639fc8d7
Sean Klein <sean@oxide.computer>
2021-05-25 09:38:22 -0400

    Simplify client bindings using reqwest

--- propolis-client/src/lib.rs
+++ propolis-client/src/lib.rs
@@ -1,87 +1,105 @@
 //! Interface for making API requests to propolis.
 //! This should be replaced with a client generated from the OpenAPI spec.
 
-use http::Method;
-use hyper::Body;
+use reqwest::Body;
 use serde::de::DeserializeOwned;
-use slog::Logger;
+use slog::{Logger, info, o};
 use std::net::SocketAddr;
+use thiserror::Error;
 use uuid::Uuid;
 
 pub mod api;
-mod http_client;
 
-use http_client::{ApiError, HttpClient};
+/// Errors which may be returend from the Propolis Client.
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("Request failed: {0}")]
+    Reqwest(#[from] reqwest::Error),
+}
 
 pub struct Client {
-    client: HttpClient,
+    client: reqwest::Client,
+    log: Logger,
+    address: SocketAddr,
 }
 
 impl Client {
     pub fn new(address: SocketAddr, log: Logger) -> Client {
-        Client { client: HttpClient::new("propolis", address, log) }
+        Client {
+            client: reqwest::Client::new(),
+            log: log.new(o!("propolis_client address" => address.to_string())),
+            address,
+        }
     }
 
-    async fn send_request_await_response<R, P>(
-        self,
-        path: P,
-        method: Method,
-        body: Body,
-    ) -> Result<R, ApiError>
-    where
-        R: DeserializeOwned,
-        P: AsRef<str>,
-    {
-        let mut response =
-            self.client.request(method.clone(), path.as_ref(), body).await?;
+    async fn get<T: DeserializeOwned>(&self, path: String, body: Option<Body>) -> Result<T, Error> {
+        info!(self.log, "GET request to {}", path);
+        let mut request = self.client.get(path);
+        if let Some(body) = body {
+            request = request.body(body);
+        }
+        request.send()
+            .await
+            .map_err(|e| Error::from(e))?
+            .json()
+            .await
+            .map_err(|e| e.into())
+    }
 
-        // TODO-robustness handle 300-level?
-        assert!(response.status().is_success());
-        let value = self
-            .client
-            .read_json::<R>(
-                &self.client.error_message_base(&method, path.as_ref()),
-                &mut response,
-            )
-            .await?;
-        Ok(value)
+    async fn put<T: DeserializeOwned>(&self, path: String, body: Option<Body>) -> Result<T, Error> {
+        info!(self.log, "PUT request to {}", path);
+        let mut request = self.client.put(path);
+        if let Some(body) = body {
+            request = request.body(body);
+        }
+        request.send()
+            .await
+            .map_err(|e| Error::from(e))?
+            .json()
+            .await
+            .map_err(|e| e.into())
     }
 
     pub async fn instance_ensure(
-        self,
-        properties: &api::InstanceProperties,
-    ) -> Result<api::InstanceEnsureResponse, ApiError> {
-        let path = format!("/instances/{}", properties.id);
-        let body = Body::from(serde_json::to_string(&properties).unwrap());
-        self.send_request_await_response(&path, Method::PUT, body).await
+        &self,
+        request: &api::InstanceEnsureRequest,
+    ) -> Result<api::InstanceEnsureResponse, Error> {
+        let path = format!("http://{}/instances/{}", self.address, request.properties.id);
+        let body = Body::from(serde_json::to_string(&request).unwrap());
+        self.put(path, Some(body)).await
     }
 
     pub async fn instance_get(
-        self,
+        &self,
         id: Uuid,
-    ) -> Result<api::InstanceGetResponse, ApiError> {
-        let path = format!("/instances/{}", id);
-        self.send_request_await_response(&path, Method::GET, Body::empty())
-            .await
+    ) -> Result<api::InstanceGetResponse, Error> {
+        let path = format!("http://{}/instances/{}", self.address, id);
+        self.get(path, None).await
     }
 
     pub async fn instance_state_put(
-        self,
+        &self,
         id: Uuid,
         state: api::InstanceStateRequested,
-    ) -> Result<(), ApiError> {
-        let path = format!("/instances/{}/state", id);
+    ) -> Result<(), Error> {
+        let path = format!("http://{}/instances/{}/state", self.address, id);
         let body = Body::from(serde_json::to_string(&state).unwrap());
-        self.send_request_await_response(&path, Method::PUT, body).await
+        let _ = self.client
+            .put(path)
+            .body(body)
+            .send()
+            .await
+            .map_err(|e| Error::from(e))?;
+        Ok(())
     }
 
     pub async fn instance_serial(
-        self,
+        &self,
         id: Uuid,
         state: api::InstanceSerialRequest,
-    ) -> Result<api::InstanceSerialResponse, ApiError> {
-        let path = format!("/instances/{}/serial", id);
+    ) -> Result<api::InstanceSerialResponse, Error> {
+        let path = format!("http://{}/instances/{}/serial", self.address, id);
         let body = Body::from(serde_json::to_string(&state).unwrap());
-        self.send_request_await_response(&path, Method::PUT, body).await
+        self.put(path, Some(body)).await
     }
 }
