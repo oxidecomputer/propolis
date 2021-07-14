99daa2b38e621c118232416b991082ce4188df92
Sean Klein <sean@oxide.computer>
2021-07-14 12:22:03 -0400

    [openapi][server] Emit OpenApi specification if requested (#23)

--- server/src/main.rs
+++ server/src/main.rs
@@ -17,12 +17,27 @@ use propolis_server::{config, server};
     name = "propolis-server",
     about = "An HTTP server providing access to Propolis"
 )]
-struct Args {
-    #[structopt(parse(from_os_str))]
-    cfg: PathBuf,
+enum Args {
+    /// Generates the OpenAPI specification.
+    OpenApi,
+    /// Runs the Propolis server.
+    Run {
+        #[structopt(parse(from_os_str))]
+        cfg: PathBuf,
 
-    #[structopt(name = "PROPOLIS_IP:PORT", parse(try_from_str))]
-    propolis_addr: SocketAddr,
+        #[structopt(name = "PROPOLIS_IP:PORT", parse(try_from_str))]
+        propolis_addr: SocketAddr,
+    },
+}
+
+pub fn run_openapi() -> Result<(), String> {
+    server::api()
+        .openapi("Oxide Propolis API", "0.0.1")
+        .description("Api for interacting with Propolis agent")
+        .contact_url("https://oxide.computer")
+        .contact_email("api@oxide.computer")
+        .write(&mut std::io::stdout())
+        .map_err(|e| e.to_string())
 }
 
 #[tokio::main]
@@ -32,23 +47,37 @@ async fn main() -> anyhow::Result<()> {
 
     // Command line arguments.
     let args = Args::from_args_safe()?;
-    let config = config::parse(&args.cfg)?;
 
-    // Dropshot configuration.
-    let config_dropshot = ConfigDropshot {
-        bind_address: args.propolis_addr,
-        ..Default::default()
-    };
-    let config_logging =
-        ConfigLogging::StderrTerminal { level: ConfigLoggingLevel::Info };
-    let log = config_logging
-        .to_logger("propolis-server")
-        .map_err(|error| anyhow!("failed to create logger: {}", error))?;
+    match args {
+        Args::OpenApi => run_openapi()
+            .map_err(|e| anyhow!("Cannot generate OpenAPI spec: {}", e)),
+        Args::Run { cfg, propolis_addr } => {
+            let config = config::parse(&cfg)?;
 
-    let context = server::Context::new(config);
-    let server =
-        HttpServerStarter::new(&config_dropshot, server::api(), context, &log)
+            // Dropshot configuration.
+            let config_dropshot = ConfigDropshot {
+                bind_address: propolis_addr,
+                ..Default::default()
+            };
+            let config_logging = ConfigLogging::StderrTerminal {
+                level: ConfigLoggingLevel::Info,
+            };
+            let log = config_logging.to_logger("propolis-server").map_err(
+                |error| anyhow!("failed to create logger: {}", error),
+            )?;
+
+            let context = server::Context::new(config);
+            let server = HttpServerStarter::new(
+                &config_dropshot,
+                server::api(),
+                context,
+                &log,
+            )
             .map_err(|error| anyhow!("Failed to start server: {}", error))?
             .start();
-    server.await.map_err(|e| anyhow!("Server exited with an error: {}", e))
+            server
+                .await
+                .map_err(|e| anyhow!("Server exited with an error: {}", e))
+        }
+    }
 }
