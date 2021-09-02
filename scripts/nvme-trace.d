348cdffd11e5335d2d8140cdd465f517e1b7de06
Luqman Aden <luqman@oxide.computer>
2021-09-01 18:17:42 -0700

    Use clause local variables instead of globals.

--- scripts/nvme-trace.d
+++ scripts/nvme-trace.d
@@ -25,24 +25,24 @@ struct io_info io[uint64_t];
 propolis$target:::nvme_read_enqueue,
 propolis$target:::nvme_write_enqueue
 {
-    cid = args[0];
-    op = (probename == "nvme_read_enqueue") ? "read" : "write";
-    io[cid].op = op;
-    io[cid].ts = timestamp;
-    io[cid].slba = args[1];
-    io[cid].nlb = args[2];
+    this->cid = args[0];
+    this->op = (probename == "nvme_read_enqueue") ? "read" : "write";
+    io[this->cid].op = this->op;
+    io[this->cid].ts = timestamp;
+    io[this->cid].slba = args[1];
+    io[this->cid].nlb = args[2];
 }
 
 propolis$target:::nvme_read_complete,
 propolis$target:::nvme_write_complete
 /io[args[0]].ts != 0/
 {
-    cid = args[0];
-    elapsed = timestamp - io[cid].ts;
-    elapsed_us = elapsed / 1000;
-    @time[io[cid].op] = quantize(elapsed_us);
+    this->cid = args[0];
+    this->elapsed = timestamp - io[this->cid].ts;
+    this->elapsed_us = this->elapsed / 1000;
+    @time[io[this->cid].op] = quantize(this->elapsed_us);
     printf("%s(cid=%u) %d blocks from LBA 0x%x in %uus\n",
-           io[cid].op, cid, io[cid].nlb, io[cid].slba, elapsed_us);
+           io[this->cid].op, this->cid, io[this->cid].nlb, io[this->cid].slba, this->elapsed_us);
 }
 
 dtrace:::END
