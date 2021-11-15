4be2ecc06f1e51070b465a755c41dcba8a0beee6
Luqman Aden <luqman@oxide.computer>
2021-11-15 15:15:38 -0800

    Flesh out the migration initiation.

--- server/src/lib/migrate.rs
+++ server/src/lib/migrate.rs
@@ -1,17 +1,61 @@
 use std::sync::Arc;
 
 use dropshot::{HttpError, HttpResponseOk, RequestContext};
-use hyper::{Body, Response};
+use hyper::{header, Body, Response, StatusCode};
 use propolis_client::api;
-use slog::{info, o};
+use slog::{error, info, o};
 use thiserror::Error;
 use uuid::Uuid;
 
 use crate::server::Context;
 
+/// Our migration protocol version
+const MIGRATION_PROTOCOL_VERION: usize = 0;
+
+/// Our migration protocol encoding
+const MIGRATION_PROTOCOL_ENCODING: ProtocolEncoding = ProtocolEncoding::Ron;
+
+/// The concatenated migration protocol-encoding-version string
+const MIGRATION_PROTOCOL_STR: &'static str = const_format::concatcp!(
+    "propolis-migrate-",
+    encoding_str(MIGRATION_PROTOCOL_ENCODING),
+    "/",
+    MIGRATION_PROTOCOL_VERION
+);
+
+/// Supported encoding formats
+enum ProtocolEncoding {
+    Ron,
+}
+
+/// Small helper function to stringify ProtocolEncoding
+const fn encoding_str(e: ProtocolEncoding) -> &'static str {
+    match e {
+        ProtocolEncoding::Ron => "ron",
+    }
+}
+
 /// Errors which may occur during the course of a migration
 #[derive(Error, Debug)]
-pub enum MigrateError {}
+pub enum MigrateError {
+    #[error("{0}")]
+    Http(#[from] hyper::Error),
+
+    #[error("couldn't establish migration connection to source instance")]
+    Initiate,
+
+    #[error("the source ({0}) and destination ({1}) instances are incompatible for migration")]
+    Incompatible(String, String),
+}
+
+impl MigrateError {
+    fn incompatible(src_protocol: &str) -> MigrateError {
+        MigrateError::Incompatible(
+            src_protocol.to_string(),
+            MIGRATION_PROTOCOL_STR.to_string(),
+        )
+    }
+}
 
 impl Into<HttpError> for MigrateError {
     fn into(self) -> HttpError {
@@ -54,5 +98,43 @@ pub async fn dest_initiate(
     );
     info!(log, "Begin migration"; "src_migrate_url" => &src_migrate_url);
 
-    todo!()
+    // Build upgrade request to the source instance
+    let req = hyper::Request::builder()
+        .uri(src_migrate_url)
+        .header(header::CONNECTION, "upgrade")
+        // TODO: move to constant
+        .header(header::UPGRADE, MIGRATION_PROTOCOL_STR)
+        .body(Body::empty())
+        .unwrap();
+
+    // Kick off the request
+    let res = hyper::Client::new().request(req).await?;
+    if res.status() != StatusCode::SWITCHING_PROTOCOLS
+    {
+        error!(
+            log,
+            "source instance failed to switch protocols: {}",
+            res.status()
+        );
+        return Err(MigrateError::Initiate);
+    }
+    let src_protocol = res
+        .headers()
+        .get(header::UPGRADE)
+        .and_then(|hv| hv.to_str().ok())
+        .ok_or_else(|| MigrateError::incompatible("<unknown>"))?;
+
+    // TODO: improve "negotiation"
+    if src_protocol != MIGRATION_PROTOCOL_STR {
+        error!(log, "incompatible with source instance provided protocol ({})", src_protocol);
+        return Err(MigrateError::incompatible(src_protocol));
+    }
+
+    // Now co-opt the socket for the migration protocol
+    let conn = hyper::upgrade::on(res).await?;
+
+    // Good to go, ready to migrate from the source via `conn`
+    // TODO: wrap in a tokio codec::Framed or such
+
+    Ok(HttpResponseOk(()))
 }
