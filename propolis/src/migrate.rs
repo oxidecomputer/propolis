fa0ee5fe5e702574ff05afc734250f4bdb801128
Luqman Aden <luqman@oxide.computer>
2022-03-07 02:03:05 -0800

    Add device state import method.

--- propolis/src/migrate.rs
+++ propolis/src/migrate.rs
@@ -1,11 +1,39 @@
 use crate::dispatch::DispCtx;
 
-use erased_serde::Serialize;
+use erased_serde::{Deserializer, Serialize};
 use futures::future::{self, BoxFuture};
+use thiserror::Error;
+
+/// Errors encountered while trying to export/import device state.
+#[derive(Error, Debug, serde::Deserialize, PartialEq, serde::Serialize)]
+pub enum MigrateStateError {
+    /// Encountered an error trying to deserialize the device state during import.
+    #[error("couldn't deserialize device state: {0}")]
+    ImportDeserialization(String),
+
+    /// The device doesn't implement [`Migrate::import`].
+    #[error("device state importation unimplemented")]
+    ImportUnimplmented,
+}
+
+impl From<erased_serde::Error> for MigrateStateError {
+    fn from(err: erased_serde::Error) -> Self {
+        MigrateStateError::ImportDeserialization(err.to_string())
+    }
+}
 
 pub trait Migrate: Send + Sync + 'static {
     /// Return a serialization of the current device state.
     fn export(&self, ctx: &DispCtx) -> Box<dyn Serialize>;
+
+    /// Update the current device state by using the given deserializer.
+    fn import(
+        &self,
+        _deserializer: Box<dyn Deserializer>,
+        _ctx: &DispCtx,
+    ) -> Result<(), MigrateStateError> {
+        Err(MigrateStateError::ImportUnimplmented)
+    }
 
     /// Called to indicate the device should stop servicing the
     /// guest and attempt to cancel or complete any pending operations.
