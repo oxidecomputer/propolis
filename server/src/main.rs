7f7f6e93081458bd87f83e4ece3fa1406dbb6ebe
Jordan Paige Hendricks <jordan@oxide.computer>
2022-04-15 19:24:54 +0000

    Fix failing integration test due to changed server::Context constructor.

--- server/src/main.rs
+++ server/src/main.rs
@@ -11,15 +11,12 @@ use dropshot::{
 };
 use futures::join;
 use propolis::usdt::register_probes;
-use rfb::rfb::{ProtoVersion, SecurityType, SecurityTypes};
-use rfb::server::VncServer;
-use rfb::server::{VncServerConfig, VncServerData};
-use slog::{info, o, Logger};
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use slog::info;
+use std::net::SocketAddr;
 use std::path::PathBuf;
 use structopt::StructOpt;
 
-use propolis_server::vnc::PropolisVncServer;
+use propolis_server::vnc::setup_vnc;
 use propolis_server::{config, server};
 
 #[derive(Debug, StructOpt)]
@@ -48,32 +45,6 @@ pub fn run_openapi() -> Result<(), String> {
         .contact_email("api@oxide.computer")
         .write(&mut std::io::stdout())
         .map_err(|e| e.to_string())
-}
-
-fn setup_vnc(log: &Logger) -> VncServer<PropolisVncServer> {
-    // XXX: Do we want to specify this information in the config file?
-    let initial_width = 1024;
-    let initial_height = 768;
-
-    let config = VncServerConfig {
-        addr: SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 5900),
-        version: ProtoVersion::Rfb38,
-        // vncviewer won't work without offering VncAuth, even though it doesn't ask to use
-        // it.
-        sec_types: SecurityTypes(vec![
-            SecurityType::None,
-            SecurityType::VncAuthentication,
-        ]),
-        name: "propolis-vnc".to_string(),
-    };
-    let data = VncServerData { width: initial_width, height: initial_height };
-    let pvnc = PropolisVncServer::new(
-        initial_width,
-        initial_height,
-        log.new(o!("component" => "vnc-server")),
-    );
-
-    VncServer::new(pvnc, config, data)
 }
 
 #[tokio::main]
