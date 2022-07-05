0d26810b20709b9568b00fc6b218373137ec58c3
Greg Colombo <greg@oxidecomputer.com>
2022-07-05 16:02:41 -0700

    Add custom (de)serializer and range checks for PciPath
    This lets us verify at deserialization time that a PciPath has a valid device
    and function and lets us have one canonical string form for  BDFs (previously
    you could convert from a "B.D.F" string to a PciPath, which pci::Bdf also
    allows, and you could deserialize "PciPath(B, D, F)" into a PciPath using its
    Deserialize implementation; now we just expect strings and use FromStr for
    everything).
    
    Also, add the types module to the Propolis workspace so that running cargo test
    in the root runs those unit tests.

--- types/src/lib.rs
+++ types/src/lib.rs
@@ -5,27 +5,58 @@
 //! can all use those types (and implement their own conversions to/from them)
 //! without any layering oddities.
 
-use std::io::ErrorKind;
+use std::io::{Error, ErrorKind};
 use std::str::FromStr;
 
-use serde::{Deserialize, Serialize};
+use serde::{de, Deserialize, Deserializer, Serialize, Serializer};
+
+const PCI_DEVICES_PER_BUS: u8 = 32;
+const PCI_FUNCTIONS_PER_DEVICE: u8 = 8;
 
 /// A PCI bus/device/function tuple. Supports conversion from a string formatted
 /// as "B.D.F", e.g. "0.7.0".
-#[derive(
-    Clone, Copy, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Debug,
-)]
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug)]
 pub struct PciPath(pub u8, pub u8, pub u8);
+
+impl PciPath {
+    fn new(bus: u8, device: u8, function: u8) -> Result<Self, std::io::Error> {
+        if device >= PCI_DEVICES_PER_BUS {
+            return Err(Error::new(
+                ErrorKind::InvalidInput,
+                format!(
+                    "PCI device {} outside range of 0-{}",
+                    device,
+                    PCI_DEVICES_PER_BUS - 1
+                ),
+            ));
+        }
+
+        if function >= PCI_FUNCTIONS_PER_DEVICE {
+            return Err(Error::new(
+                ErrorKind::InvalidInput,
+                format!(
+                    "PCI function {} outside range of 0-{}",
+                    function,
+                    PCI_FUNCTIONS_PER_DEVICE - 1
+                ),
+            ));
+        }
+
+        Ok(Self(bus, device, function))
+    }
+}
 
 impl FromStr for PciPath {
     type Err = std::io::Error;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut fields = Vec::with_capacity(3);
         for f in s.split('.') {
-            fields.push(
-                u8::from_str(f)
-                    .map_err(|e| Self::Err::new(ErrorKind::InvalidInput, e))?,
-            );
+            fields.push(u8::from_str(f).map_err(|e| {
+                Self::Err::new(
+                    ErrorKind::InvalidInput,
+                    format!("Failed to parse PCI path {}: {}", s, e),
+                )
+            })?);
         }
 
         if fields.len() != 3 {
@@ -39,27 +70,81 @@ impl FromStr for PciPath {
             ));
         }
 
-        Ok(PciPath(fields[0], fields[1], fields[2]))
+        Self::new(fields[0], fields[1], fields[2])
+    }
+}
+
+impl Serialize for PciPath {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer
+            .serialize_str(format!("{}.{}.{}", self.0, self.1, self.2).as_str())
+    }
+}
+
+impl<'d> Deserialize<'d> for PciPath {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'d>,
+    {
+        let s = String::deserialize(deserializer)?;
+        FromStr::from_str(&s).map_err(de::Error::custom)
     }
 }
 
 #[cfg(test)]
 mod test {
     use super::PciPath;
+    use serde::Deserialize;
+    use serde_test::{assert_tokens, Token};
     use std::str::FromStr;
 
+    const TEST_CASES: &[(&str, Result<PciPath, ()>)] = &[
+        ("0.7.0", Ok(PciPath(0, 7, 0))),
+        ("1.2.3", Ok(PciPath(1, 2, 3))),
+        ("0.40.0", Err(())),
+        ("0.1.9", Err(())),
+        ("255.254.253", Err(())),
+        ("1000.0.0", Err(())),
+        ("4/3/4", Err(())),
+        ("a.b.c", Err(())),
+        ("1.5#4", Err(())),
+        ("", Err(())),
+        ("alas, poor PCI device", Err(())),
+    ];
+
     #[test]
-    fn pci_path_conversions() {
-        assert_eq!(PciPath::from_str("0.7.0").unwrap(), PciPath(0, 7, 0));
-        assert_eq!(
-            PciPath::from_str("255.254.253").unwrap(),
-            PciPath(255, 254, 253)
-        );
-        assert!(PciPath::from_str("1000.0.0").is_err());
-        assert!(PciPath::from_str("4/3/4").is_err());
-        assert!(PciPath::from_str("a.b.c").is_err());
-        assert!(PciPath::from_str("1.5#4").is_err());
-        assert!(PciPath::from_str("").is_err());
-        assert!(PciPath::from_str("alas, poor PCI device").is_err());
+    fn pci_path_from_str() {
+        for (input, expected) in TEST_CASES {
+            match PciPath::from_str(input) {
+                Ok(path) => assert_eq!(path, expected.unwrap()),
+                Err(_) => assert!(
+                    expected.is_err(),
+                    "Expected error parsing PCI path {}",
+                    input
+                ),
+            }
+        }
+    }
+
+    #[test]
+    fn pci_path_serialization() {
+        for (input, expected) in TEST_CASES {
+            match expected {
+                Ok(path) => {
+                    assert_tokens(path, &[Token::Str(input)]);
+                }
+                Err(_) => {
+                    // Manually deserialize instead of using
+                    // serde_test::assert_tokens_de_error to avoid having to
+                    // specify exact error messages.
+                    let tokens = [Token::Str(input)];
+                    let mut de = serde_test::Deserializer::new(&tokens);
+                    assert!(PciPath::deserialize(&mut de).is_err());
+                }
+            }
+        }
     }
 }
