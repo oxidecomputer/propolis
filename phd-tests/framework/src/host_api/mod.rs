348076ade3a2c72d4244d456d1082a1f3a04a367
Greg Colombo <greg@oxidecomputer.com>
2022-08-23 16:54:18 +0000

    Clean up illumos-only code inclusion

--- phd-tests/framework/src/host_api/mod.rs
+++ phd-tests/framework/src/host_api/mod.rs
@@ -1,11 +1,9 @@
-#[cfg(target_os = "illumos")]
-mod kvm;
-
-#[cfg(target_os = "illumos")]
-pub use kvm::*;
-
-#[cfg(not(target_os = "illumos"))]
-mod stubs;
-
-#[cfg(not(target_os = "illumos"))]
-pub use stubs::*;
+cfg_if::cfg_if! {
+    if #[cfg(target_os = "illumos")] {
+        mod kvm;
+        pub use kvm::*;
+    } else {
+        mod stubs;
+        pub use stubs::*;
+    }
+}
