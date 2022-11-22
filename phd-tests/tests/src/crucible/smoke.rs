2f65d07431bfa90846557e4bdb445d365d1629ee
Greg Colombo <greg@oxidecomputer.com>
2022-11-22 19:04:51 +0000

    Add data persistence smoke test

--- phd-tests/tests/src/crucible/smoke.rs
+++ phd-tests/tests/src/crucible/smoke.rs
@@ -1,4 +1,7 @@
+use std::time::Duration;
+
 use phd_testcase::{phd_framework::test_vm::vm_config::DiskInterface, *};
+use propolis_client::handmade::api::InstanceState;
 
 #[phd_testcase]
 fn boot_test(ctx: &TestContext) {
@@ -12,4 +15,47 @@ fn boot_test(ctx: &TestContext) {
     let mut vm = ctx.vm_factory.new_vm("crucible_boot_test", config)?;
     vm.launch()?;
     vm.wait_to_boot()?;
+}
+
+#[phd_testcase]
+fn shutdown_persistence_test(ctx: &TestContext) {
+    let disk = super::create_default_boot_disk(ctx, 10)?;
+    disk.set_generation(1);
+    let config = ctx.deviceless_vm_config().set_boot_disk(
+        disk.clone(),
+        4,
+        DiskInterface::Nvme,
+    );
+
+    let mut vm =
+        ctx.vm_factory.new_vm("crucible_shutdown_persistence_test", config)?;
+    vm.launch()?;
+    vm.wait_to_boot()?;
+
+    // Verify that the test file doesn't exist yet, then touch it, flush it, and
+    // shut down the VM.
+    let lsout = vm.run_shell_command("ls foo.bar 2> /dev/null")?;
+    assert_eq!(lsout, "");
+    vm.run_shell_command("touch ./foo.bar")?;
+    vm.run_shell_command("sync ./foo.bar")?;
+    vm.stop()?;
+    vm.wait_for_state(InstanceState::Destroyed, Duration::from_secs(60))?;
+
+    // Increment the disk's generation before attaching it to a new VM.
+    disk.set_generation(2);
+    let config = ctx.deviceless_vm_config().set_boot_disk(
+        disk.clone(),
+        4,
+        DiskInterface::Nvme,
+    );
+
+    // The touched file from the previous VM should be present in the new one.
+    let mut vm = ctx
+        .vm_factory
+        .new_vm("crucible_shutdown_persistence_test_2", config)?;
+
+    vm.launch()?;
+    vm.wait_to_boot()?;
+    let lsout = vm.run_shell_command("ls foo.bar 2> /dev/null")?;
+    assert_eq!(lsout, "foo.bar");
 }
