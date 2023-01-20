46e8b9c6b54aa841510a4dff424b0f69a1e994d4
lif <>
2023-01-20 07:03:15 +0000

    wip: make a mock-only feature flag for including in omicron tests without weird dependency cycles

--- bin/propolis-server/src/lib/lib.rs
+++ bin/propolis-server/src/lib/lib.rs
@@ -1,11 +1,21 @@
+// always present
 pub mod config;
-mod initializer;
-mod migrate;
 pub mod mock_server;
 mod serial;
-pub mod server;
+#[cfg_attr(feature = "mock-only", allow(unused))]
 mod spec;
+
+#[cfg(not(feature = "mock-only"))]
+mod initializer;
+#[cfg(not(feature = "mock-only"))]
+mod migrate;
+#[cfg(not(feature = "mock-only"))]
+pub mod server;
+#[cfg(not(feature = "mock-only"))]
 mod stats;
+#[cfg(not(feature = "mock-only"))]
 mod vcpu_tasks;
+#[cfg(not(feature = "mock-only"))]
 mod vm;
+#[cfg(not(feature = "mock-only"))]
 pub mod vnc;
