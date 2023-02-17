40e8d8e280c312e44047ff9d6b3b261e4537977a
lif <unknown>
2023-02-17 08:44:32 +0000

    tidy up conditional imports

--- bin/propolis-server/src/lib/lib.rs
+++ bin/propolis-server/src/lib/lib.rs
@@ -1,21 +1,19 @@
 // always present
 pub mod config;
+#[cfg(any(test, feature = "mock-only"))]
 pub mod mock_server;
 mod serial;
 #[cfg_attr(feature = "mock-only", allow(unused))]
 mod spec;
 
-#[cfg(not(feature = "mock-only"))]
-mod initializer;
-#[cfg(not(feature = "mock-only"))]
-mod migrate;
-#[cfg(not(feature = "mock-only"))]
-pub mod server;
-#[cfg(not(feature = "mock-only"))]
-mod stats;
-#[cfg(not(feature = "mock-only"))]
-mod vcpu_tasks;
-#[cfg(not(feature = "mock-only"))]
-mod vm;
-#[cfg(not(feature = "mock-only"))]
-pub mod vnc;
+cfg_if::cfg_if! {
+    if #[cfg(not(feature = "mock-only"))] {
+        mod initializer;
+        mod migrate;
+        pub mod server;
+        mod stats;
+        mod vcpu_tasks;
+        mod vm;
+        pub mod vnc;
+    }
+}
