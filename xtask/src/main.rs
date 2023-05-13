c81fc5cc7215cc9e1acf5a257b7743bdeea510f6
Patrick Mooney <pmooney@pfmooney.com>
2023-05-13 01:50:19 -0500

    Bolster Clippy checks for codebase
    With so many different components, some not build by default in the
    workspace, it has become easy to overlook problems which would otherwise
    be highlighted by Clippy.  Add an xtask to check the entire codebase and
    wire it into the GH Actions CI.

---
+++ xtask/src/main.rs
@@ -0,0 +1,84 @@
+use std::process::{Command, Stdio};
+
+use anyhow::{bail, Result};
+use clap::{Parser, Subcommand};
+use serde_json::{Map, Value};
+
+#[derive(Parser)]
+#[command(name = "cargo xtask", about = "Builder tasks for Propolis")]
+struct Args {
+    #[command(subcommand)]
+    cmd: Cmds,
+}
+
+#[derive(Subcommand)]
+enum Cmds {
+    /// Run suite of clippy checks
+    Clippy {
+        /// Treat warnings as errors
+        #[arg(short, long)]
+        strict: bool,
+    },
+}
+
+fn workspace_root() -> Result<String> {
+    let mut cmd = Command::new("cargo");
+    cmd.args(["metadata", "--format-version=1"])
+        .stdin(Stdio::null())
+        .stderr(Stdio::inherit());
+
+    let output = cmd.output()?;
+    if !output.status.success() {
+        bail!("failed to query cargo metadata");
+    }
+    let metadata: Map<String, Value> = serde_json::from_slice(&output.stdout)?;
+
+    if let Some(Value::String(root)) = metadata.get("workspace_root") {
+        Ok(root.clone())
+    } else {
+        bail!("could not location workspace root")
+    }
+}
+
+fn cmd_clippy(strict: bool) -> Result<()> {
+    let wroot = workspace_root()?;
+
+    let run_clippy = |args: &[&str]| -> Result<bool> {
+        let mut cmd = Command::new("cargo");
+        cmd.arg("clippy").arg("--no-deps").args(args).current_dir(&wroot);
+
+        if strict {
+            cmd.args(["--", "-Dwarnings"]);
+        }
+
+        let status = cmd.spawn()?.wait()?;
+        Ok(!status.success())
+    };
+
+    let mut failed = false;
+
+    // Everything in the workspace
+    failed |= run_clippy(&["--workspace"])?;
+
+    // And try with tests too
+    failed |= run_clippy(&["--workspace", "--tests"])?;
+
+    // Check the Falcon bits
+    failed |= run_clippy(&["-p", "propolis-server", "--features", "falcon"])?;
+
+    // Check the mock server
+    failed |=
+        run_clippy(&["-p", "propolis-server", "--features", "mock-only"])?;
+
+    if failed {
+        bail!("Clippy failures detected")
+    }
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    match Args::parse().cmd {
+        Cmds::Clippy { strict } => cmd_clippy(strict),
+    }
+}
