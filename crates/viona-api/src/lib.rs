c791ec3c6bd71d6722c9e39a1ec57f20d7081e5a
Nils Nieuwejaar <nils@oxidecomputer.com>
2024-05-23 18:52:38 +0000

    add support for vlans in falcon

--- crates/viona-api/src/lib.rs
+++ crates/viona-api/src/lib.rs
@@ -19,6 +19,11 @@ impl VionaFd {
 
         let mut vna_create = vioc_create { c_linkid: link_id, c_vmfd: vm_fd };
         let _ = unsafe { this.ioctl(ioctls::VNA_IOC_CREATE, &mut vna_create) }?;
+        #[cfg(feature = "falcon")]
+        let _ = this.ioctl_usize(
+            ioctls::VNA_IOC_SET_PROMISC,
+            vioc_promisc::VIONA_PROMISC_ALL_VLAN as usize,
+        )?;
         Ok(this)
     }
 
@@ -76,6 +81,7 @@ impl VionaFd {
                 | ioctls::VNA_IOC_RING_PAUSE
                 | ioctls::VNA_IOC_RING_INTR_CLR
                 | ioctls::VNA_IOC_VERSION
+                | ioctls::VNA_IOC_SET_PROMISC
         )
     }
 }
