516dabe473cecdc3baea1db98a80441968c144cf
Greg Colombo <greg@oxidecomputer.com>
2024-09-18 11:50:55 -0700

    instance spec rework: move migration compat checks to propolis-server (#766)
    Transfer Propolis's component-level live migration compatibility checks
    from the API types crate to propolis-server. This is the last big chunk of
    application code that lives in the api-types crate. This is not straight-up
    code movement because the internal Spec type doesn't have the same notion
    of "device" and "backend" collections that the wire type has. This means
    that the hierarchy of errors in the new propolis-server compat.rs is
    different (but hopefully simpler) than what was in the old
    propolis-api-types migration.rs. The actual compatibility rules should be
    the same as they were before, however, and I've tried to bring over
    wholesale the api-types crate's compatibility unit tests.
    
    Also, adjust how compatibility checks work in the migration protocol.
    Before, the migration preamble sent a DeviceSpecV0 and a list of backend
    component names; the destination checked that the devices were compatible
    and that the backend name sets matched (but, owing to the absence of the
    actual backend descriptors, did not check that the backends themselves were
    migration-compatible). With this change, the preamble contains the source's
    entire InstanceSpecV0, though the compatibility checks are still
    (currently) limited to the VM's devices. This breaks the protocol's
    dependency on the notion that instance specs necessarily have collections
    of "devices" and "backends" as opposed to simply having a set of
    components. This is a prerequisite for the next change in this series, which
    removes this distinction from the InstanceSpecV0 type itself.

--- crates/propolis-api-types/src/instance_spec/components/devices.rs
+++ crates/propolis-api-types/src/instance_spec/components/devices.rs
@@ -5,35 +5,9 @@
 //! Device configuration data: components that define VM properties that are
 //! visible to a VM's guest software.
 
-use crate::instance_spec::{migration::MigrationElement, PciPath};
+use crate::instance_spec::PciPath;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
-use thiserror::Error;
-
-fn backend_name_matches(
-    this: &str,
-    other: &str,
-) -> Result<(), MigrationCompatibilityError> {
-    if this != other {
-        Err(MigrationCompatibilityError::BackendNameMismatch(
-            this.to_owned(),
-            other.to_owned(),
-        ))
-    } else {
-        Ok(())
-    }
-}
-
-fn pci_path_matches(
-    this: &PciPath,
-    other: &PciPath,
-) -> Result<(), MigrationCompatibilityError> {
-    if this != other {
-        Err(MigrationCompatibilityError::PciPath(*this, *other))
-    } else {
-        Ok(())
-    }
-}
 
 /// A disk that presents a virtio-block interface to the guest.
 #[derive(Clone, Deserialize, Serialize, Debug, JsonSchema)]
@@ -46,22 +20,6 @@ pub struct VirtioDisk {
     pub pci_path: PciPath,
 }
 
-impl MigrationElement for VirtioDisk {
-    fn kind(&self) -> &'static str {
-        "VirtioDisk"
-    }
-
-    fn can_migrate_from_element(
-        &self,
-        other: &Self,
-    ) -> Result<(), crate::instance_spec::migration::ElementCompatibilityError>
-    {
-        backend_name_matches(&self.backend_name, &other.backend_name)?;
-        pci_path_matches(&self.pci_path, &other.pci_path)?;
-        Ok(())
-    }
-}
-
 /// A disk that presents an NVMe interface to the guest.
 #[derive(Clone, Deserialize, Serialize, Debug, JsonSchema)]
 #[serde(deny_unknown_fields)]
@@ -71,22 +29,6 @@ pub struct NvmeDisk {
 
     /// The PCI bus/device/function at which this disk should be attached.
     pub pci_path: PciPath,
-}
-
-impl MigrationElement for NvmeDisk {
-    fn kind(&self) -> &'static str {
-        "NvmeDisk"
-    }
-
-    fn can_migrate_from_element(
-        &self,
-        other: &Self,
-    ) -> Result<(), crate::instance_spec::migration::ElementCompatibilityError>
-    {
-        backend_name_matches(&self.backend_name, &other.backend_name)?;
-        pci_path_matches(&self.pci_path, &other.pci_path)?;
-        Ok(())
-    }
 }
 
 /// A network card that presents a virtio-net interface to the guest.
@@ -104,22 +46,6 @@ pub struct VirtioNic {
 
     /// The PCI path at which to attach this device.
     pub pci_path: PciPath,
-}
-
-impl MigrationElement for VirtioNic {
-    fn kind(&self) -> &'static str {
-        "VirtioNic"
-    }
-
-    fn can_migrate_from_element(
-        &self,
-        other: &Self,
-    ) -> Result<(), crate::instance_spec::migration::ElementCompatibilityError>
-    {
-        backend_name_matches(&self.backend_name, &other.backend_name)?;
-        pci_path_matches(&self.pci_path, &other.pci_path)?;
-        Ok(())
-    }
 }
 
 /// A serial port identifier, which determines what I/O ports a guest can use to
@@ -145,28 +71,6 @@ pub struct SerialPort {
     pub num: SerialPortNumber,
 }
 
-impl MigrationElement for SerialPort {
-    fn kind(&self) -> &'static str {
-        "SerialPort"
-    }
-
-    fn can_migrate_from_element(
-        &self,
-        other: &Self,
-    ) -> Result<(), crate::instance_spec::migration::ElementCompatibilityError>
-    {
-        if self != other {
-            Err(MigrationCompatibilityError::ComponentConfiguration(format!(
-                "serial port number mismatch (self: {0:?}, other: {1:?})",
-                self, other
-            ))
-            .into())
-        } else {
-            Ok(())
-        }
-    }
-}
-
 /// A PCI-PCI bridge.
 #[derive(
     Clone, Copy, Deserialize, Serialize, Debug, PartialEq, Eq, JsonSchema,
@@ -180,44 +84,6 @@ pub struct PciPciBridge {
 
     /// The PCI path at which to attach this bridge.
     pub pci_path: PciPath,
-}
-
-impl MigrationElement for PciPciBridge {
-    fn kind(&self) -> &'static str {
-        "PciPciBridge"
-    }
-
-    fn can_migrate_from_element(
-        &self,
-        other: &Self,
-    ) -> Result<(), crate::instance_spec::migration::ElementCompatibilityError>
-    {
-        pci_path_matches(&self.pci_path, &other.pci_path)?;
-        if self.downstream_bus != other.downstream_bus {
-            Err(MigrationCompatibilityError::ComponentConfiguration(format!(
-                "bridge downstream bus mismatch (self: {0}, other: {1})",
-                self.downstream_bus, other.downstream_bus
-            ))
-            .into())
-        } else {
-            Ok(())
-        }
-    }
-}
-
-#[derive(Debug, Error)]
-pub enum MigrationCompatibilityError {
-    /// The two devices have mismatched backend names. This means that migration
-    /// might fail to copy the source device's backend's state to the
-    /// corresponding target backend.
-    #[error("devices have different backend names (self: {0}, other: {1})")]
-    BackendNameMismatch(String, String),
-
-    #[error("PCI devices have different paths (self: {0:?}, other: {1:?})")]
-    PciPath(PciPath, PciPath),
-
-    #[error("component configurations incompatible: {0}")]
-    ComponentConfiguration(String),
 }
 
 #[derive(
@@ -236,28 +102,6 @@ pub struct QemuPvpanic {
     /// Enable the QEMU PVPANIC ISA bus device (I/O port 0x505).
     pub enable_isa: bool,
     // TODO(eliza): add support for the PCI PVPANIC device...
-}
-
-impl MigrationElement for Option<QemuPvpanic> {
-    fn kind(&self) -> &'static str {
-        "QemuPvpanic"
-    }
-
-    fn can_migrate_from_element(
-        &self,
-        other: &Self,
-    ) -> Result<(), crate::instance_spec::migration::ElementCompatibilityError>
-    {
-        if self != other {
-            Err(MigrationCompatibilityError::ComponentConfiguration(format!(
-                "pvpanic configuration mismatch (self: {0:?}, other: {1:?})",
-                self, other
-            ))
-            .into())
-        } else {
-            Ok(())
-        }
-    }
 }
 
 //
@@ -307,149 +151,4 @@ pub struct P9fs {
 
     /// The PCI path at which to attach the guest to this P9 filesystem.
     pub pci_path: PciPath,
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn compatible_virtio_disk() {
-        let d1 = VirtioDisk {
-            backend_name: "storage_backend".to_string(),
-            pci_path: PciPath::new(0, 5, 0).unwrap(),
-        };
-        assert!(d1.can_migrate_from_element(&d1).is_ok());
-    }
-
-    #[test]
-    fn incompatible_virtio_disk() {
-        let d1 = VirtioDisk {
-            backend_name: "storage_backend".to_string(),
-            pci_path: PciPath::new(0, 5, 0).unwrap(),
-        };
-
-        let d2 = VirtioDisk { backend_name: "other_backend".to_string(), ..d1 };
-        assert!(d1.can_migrate_from_element(&d2).is_err());
-
-        let d2 = VirtioDisk {
-            pci_path: PciPath::new(0, 6, 0).unwrap(),
-            ..d1.clone()
-        };
-        assert!(d1.can_migrate_from_element(&d2).is_err());
-    }
-
-    #[test]
-    fn compatible_nvme_disk() {
-        let d1 = NvmeDisk {
-            backend_name: "storage_backend".to_string(),
-            pci_path: PciPath::new(0, 5, 0).unwrap(),
-        };
-        assert!(d1.can_migrate_from_element(&d1).is_ok());
-    }
-
-    #[test]
-    fn incompatible_nvme_disk() {
-        let d1 = NvmeDisk {
-            backend_name: "storage_backend".to_string(),
-            pci_path: PciPath::new(0, 5, 0).unwrap(),
-        };
-
-        let d2 = NvmeDisk { backend_name: "other_backend".to_string(), ..d1 };
-        assert!(d1.can_migrate_from_element(&d2).is_err());
-
-        let d2 =
-            NvmeDisk { pci_path: PciPath::new(0, 6, 0).unwrap(), ..d1.clone() };
-        assert!(d1.can_migrate_from_element(&d2).is_err());
-    }
-
-    #[test]
-    fn compatible_virtio_nic() {
-        let d1 = VirtioNic {
-            backend_name: "storage_backend".to_string(),
-            interface_id: uuid::Uuid::new_v4(),
-            pci_path: PciPath::new(0, 5, 0).unwrap(),
-        };
-        assert!(d1.can_migrate_from_element(&d1).is_ok());
-    }
-
-    #[test]
-    fn incompatible_virtio_nic() {
-        let d1 = VirtioNic {
-            backend_name: "storage_backend".to_string(),
-            interface_id: uuid::Uuid::new_v4(),
-            pci_path: PciPath::new(0, 5, 0).unwrap(),
-        };
-
-        let d2 = VirtioNic { backend_name: "other_backend".to_string(), ..d1 };
-        assert!(d1.can_migrate_from_element(&d2).is_err());
-
-        let d2 = VirtioNic {
-            pci_path: PciPath::new(0, 6, 0).unwrap(),
-            ..d1.clone()
-        };
-        assert!(d1.can_migrate_from_element(&d2).is_err());
-    }
-
-    #[test]
-    fn serial_port_compatibility() {
-        let ports = [
-            SerialPortNumber::Com1,
-            SerialPortNumber::Com2,
-            SerialPortNumber::Com3,
-            SerialPortNumber::Com4,
-        ];
-
-        for (p1, p2) in
-            ports.into_iter().flat_map(|p| std::iter::repeat(p).zip(ports))
-        {
-            let can_migrate = SerialPort { num: p1 }
-                .can_migrate_from_element(&SerialPort { num: p2 });
-
-            assert_eq!(
-                p1 == p2,
-                can_migrate.is_ok(),
-                "p1: {:?}, p2: {:?}",
-                p1,
-                p2
-            );
-        }
-    }
-
-    #[test]
-    fn pci_bridge_compatibility() {
-        let b1 = PciPciBridge {
-            downstream_bus: 1,
-            pci_path: PciPath::new(1, 2, 3).unwrap(),
-        };
-
-        let mut b2 = b1;
-        assert!(b1.can_migrate_from_element(&b2).is_ok());
-
-        b2.downstream_bus += 1;
-        assert!(b1.can_migrate_from_element(&b2).is_err());
-        b2.downstream_bus = b1.downstream_bus;
-
-        b2.pci_path = PciPath::new(4, 5, 6).unwrap();
-        assert!(b1.can_migrate_from_element(&b2).is_err());
-    }
-
-    #[test]
-    fn incompatible_qemu_pvpanic() {
-        let d1 = Some(QemuPvpanic { enable_isa: true });
-        let d2 = Some(QemuPvpanic { enable_isa: false });
-        assert!(d1.can_migrate_from_element(&d2).is_err());
-        assert!(d1.can_migrate_from_element(&None).is_err());
-    }
-
-    #[test]
-    fn compatible_qemu_pvpanic() {
-        let d1 = Some(QemuPvpanic { enable_isa: true });
-        let d2 = Some(QemuPvpanic { enable_isa: true });
-        assert!(d1.can_migrate_from_element(&d2).is_ok());
-
-        let d1 = Some(QemuPvpanic { enable_isa: false });
-        let d2 = Some(QemuPvpanic { enable_isa: false });
-        assert!(d1.can_migrate_from_element(&d2).is_ok());
-    }
 }
