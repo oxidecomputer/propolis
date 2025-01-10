5b4e8c9c55c938982bc859cd192ebae52fa87fda
Greg Colombo <greg@oxidecomputer.com>
2025-01-10 19:03:38 +0000

    format comments

--- crates/propolis-api-types/src/lib.rs
+++ crates/propolis-api-types/src/lib.rs
@@ -287,28 +287,30 @@ pub struct Instance {
 /// Request a specific range of an Instance's serial console output history.
 #[derive(Clone, Debug, Deserialize, Serialize, JsonSchema, PartialEq)]
 pub struct InstanceSerialConsoleHistoryRequest {
-    /// Character index in the serial buffer from which to read, counting the bytes output since
-    /// instance start. If this is not provided, `most_recent` must be provided, and if this *is*
-    /// provided, `most_recent` must *not* be provided.
+    /// Character index in the serial buffer from which to read, counting the
+    /// bytes output since instance start. If this is not provided,
+    /// `most_recent` must be provided, and if this *is* provided, `most_recent`
+    /// must *not* be provided.
     pub from_start: Option<u64>,
-    /// Character index in the serial buffer from which to read, counting *backward* from the most
-    /// recently buffered data retrieved from the instance. (See note on `from_start` about mutual
-    /// exclusivity)
+    /// Character index in the serial buffer from which to read, counting
+    /// *backward* from the most recently buffered data retrieved from the
+    /// instance. (See note on `from_start` about mutual exclusivity)
     pub most_recent: Option<u64>,
-    /// Maximum number of bytes of buffered serial console contents to return. If the requested
-    /// range runs to the end of the available buffer, the data returned will be shorter than
-    /// `max_bytes`.
+    /// Maximum number of bytes of buffered serial console contents to return.
+    /// If the requested range runs to the end of the available buffer, the data
+    /// returned will be shorter than `max_bytes`.
     pub max_bytes: Option<u64>,
 }
 
 /// Contents of an Instance's serial console buffer.
 #[derive(Clone, Debug, Deserialize, Serialize, JsonSchema)]
 pub struct InstanceSerialConsoleHistoryResponse {
-    /// The bytes starting from the requested offset up to either the end of the buffer or the
-    /// request's `max_bytes`. Provided as a u8 array rather than a string, as it may not be UTF-8.
+    /// The bytes starting from the requested offset up to either the end of the
+    /// buffer or the request's `max_bytes`. Provided as a u8 array rather than
+    /// a string, as it may not be UTF-8.
     pub data: Vec<u8>,
-    /// The absolute offset since boot (suitable for use as `byte_offset` in a subsequent request)
-    /// of the last byte returned in `data`.
+    /// The absolute offset since boot (suitable for use as `byte_offset` in a
+    /// subsequent request) of the last byte returned in `data`.
     pub last_byte_offset: u64,
 }
 
@@ -316,14 +318,16 @@ pub struct InstanceSerialConsoleHistoryResponse {
 /// bytes from the buffered history first.
 #[derive(Clone, Debug, Deserialize, Serialize, JsonSchema, PartialEq)]
 pub struct InstanceSerialConsoleStreamRequest {
-    /// Character index in the serial buffer from which to read, counting the bytes output since
-    /// instance start. If this is provided, `most_recent` must *not* be provided.
-    // TODO: if neither is specified, send enough serial buffer history to reconstruct
-    //  the current contents and cursor state of an interactive terminal
+    /// Character index in the serial buffer from which to read, counting the
+    /// bytes output since instance start. If this is provided, `most_recent`
+    /// must *not* be provided.
+    // TODO: if neither is specified, send enough serial buffer history to
+    // reconstruct the current contents and cursor state of an interactive
+    // terminal
     pub from_start: Option<u64>,
-    /// Character index in the serial buffer from which to read, counting *backward* from the most
-    /// recently buffered data retrieved from the instance. (See note on `from_start` about mutual
-    /// exclusivity)
+    /// Character index in the serial buffer from which to read, counting
+    /// *backward* from the most recently buffered data retrieved from the
+    /// instance. (See note on `from_start` about mutual exclusivity)
     pub most_recent: Option<u64>,
     /// True if the connection should allow writing. If this option is set, any
     /// existing writer to the serial console will be disconnected when this
