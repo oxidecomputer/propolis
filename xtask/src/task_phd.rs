8e1bb48dba5b886b7b4512d4c63be5e09b518d2b
iximeow <iximeow@oxide.computer>
2025-02-20 18:22:19 +0000

    after clippying, now rustfmt

--- xtask/src/task_phd.rs
+++ xtask/src/task_phd.rs
@@ -301,9 +301,8 @@ impl Cmd {
             }
         };
 
-        let artifact_dir = artifact_args
-            .artifact_directory
-            .unwrap_or_else(|| {
+        let artifact_dir =
+            artifact_args.artifact_directory.unwrap_or_else(|| {
                 // if there's no explicitly overridden `artifact_dir` path, use
                 // `target/phd/artifacts`.
                 phd_dir.join("artifacts")
@@ -331,9 +330,8 @@ impl Cmd {
 
         mkdir(&tmp_dir, "temp directory")?;
 
-        let artifacts_toml = artifact_args
-            .artifact_toml_path
-            .unwrap_or_else(|| {
+        let artifacts_toml =
+            artifact_args.artifact_toml_path.unwrap_or_else(|| {
                 // if there's no explicitly overridden `artifacts.toml` path,
                 // determine the default one from the workspace path.
                 relativize(&meta.workspace_root)
