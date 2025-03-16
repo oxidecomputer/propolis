45ab43770b5d5eaa3d5a6e102b347ae78be32469
iximeow <iximeow@oxide.computer>
2025-03-16 20:02:34 +0000

    other image is too large, accept booting to prompt
    when i tried using this image locally, though, it seemed to want
    credentials to log in. very confused. we'll see what this does.
    
    the other windows image that does work is huge (30gb) and the Crucible
    downstairs would need to move off of /tmp. i'd rather make a smaller
    phd-purpose Windows image. will get there when i get there.

--- phd-tests/framework/src/guest_os/windows.rs
+++ phd-tests/framework/src/guest_os/windows.rs
@@ -114,12 +114,7 @@ pub(super) fn get_login_sequence_for<'a>(
             "Use any other key to view this channel.",
         ),
         CommandSequenceEntry::write_str(""),
-        CommandSequenceEntry::wait_for("Username:"),
-        CommandSequenceEntry::write_str("Administrator"),
-        CommandSequenceEntry::wait_for("Domain  :"),
-        CommandSequenceEntry::write_str(""),
-        CommandSequenceEntry::wait_for("Password:"),
-        CommandSequenceEntry::write_str("0xide#1Fan"),
+        CommandSequenceEntry::wait_for("C:\\Windows\\system32>"),
     ];
 
     // Earlier Windows Server versions' serial console-based command prompts
