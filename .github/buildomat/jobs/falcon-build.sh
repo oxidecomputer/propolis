02fdf06bb279fc1b1393f993b90cbe84b7e9f281
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2025-04-14 10:52:34 -0700

    add digests to falcon artifacts (#895)

--- .github/buildomat/jobs/falcon-build.sh
+++ .github/buildomat/jobs/falcon-build.sh
@@ -12,11 +12,21 @@
 #: series = "falcon"
 #: name = "propolis-server"
 #: from_output = "/work/release/propolis-server"
+#: 
+#: [[publish]]
+#: series = "falcon"
+#: name = "propolis-server.sha256.txt"
+#: from_output = "/work/release/propolis-server.sha256.txt"
 #:
 #: [[publish]]
 #: series = "falcon"
 #: name = "propolis-cli"
 #: from_output = "/work/release/propolis-cli"
+#:
+#: [[publish]]
+#: series = "falcon"
+#: name = "propolis-cli.sha256.txt"
+#: from_output = "/work/release/propolis-cli.sha256.txt"
 
 set -o errexit
 set -o pipefail
@@ -37,3 +47,7 @@ OUTDIR=/work/release
 mkdir -p $OUTDIR
 cp target/release/propolis-cli $OUTDIR/propolis-cli
 cp target/release/propolis-server $OUTDIR/propolis-server
+
+cd $OUTDIR
+digest -a sha256 propolis-cli > propolis-cli.sha256.txt
+digest -a sha256 propolis-server > propolis-server.sha256.txt
