f85be0acedd1bff1433f09b3a1c3e76ccaf62b56
iximeow <iximeow@oxide.computer>
2025-08-20 19:06:01 -0700

    add a D script to report VMs' CPUID queries (#934)
    it'd be nice to elide the subleaf for leaves that do not have subleaves, but that more careful treatment of CPUID leaves is motivated elsewhere first (e.g. actual usage in Propolis) and might end up here via automatic translation or something later on.

---
+++ scripts/cpuid-queries.d
@@ -0,0 +1,53 @@
+#pragma D option defaultargs
+#pragma D option quiet
+
+/*
+ * Report guest CPUID queries and returned leaves.
+ *
+ * Usage: ./cpuid-queries.d <target VM's struct vm*>
+ *
+ * You probably have a VM name you care about, not its `struct vm*`. How do you
+ * go from the name to the pointer required here? And why?
+ *
+ * To the first question, something like this will get you the `vmm_vm` pointer
+ * to use with this script:
+ * ```
+ * mdb -ke '::walk vmm | ::print struct vmm_softc vmm_vm vmm_name ! grep -B 1 <VM_NAME>'
+ * ```
+ *
+ * Why? Because the VM's name is on vmm_softc, which has a reference to the
+ * structure used in CPUID emulation and so on, but there's no back reference.
+ * So this is slightly less convoluted than capturing the `struct vm` pointer at
+ * some earlier point when we still have a softc. This may well get simplified
+ * with a backref in the future.
+ */
+
+BEGIN {
+	if ($$1 == "") {
+		printf("target `struct vm*` required");
+		exit(1);
+	}
+
+	target_vm = $1;
+}
+
+fbt::vcpu_emulate_cpuid:entry / arg0 == target_vm / {
+	self->rax = (uint64_t*)arg2;
+	self->rbx = (uint64_t*)arg3;
+	self->rcx = (uint64_t*)arg4;
+	self->rdx = (uint64_t*)arg5;
+	self->leaf = (uint32_t)*self->rax;
+	self->subleaf = (uint32_t)*self->rcx;
+}
+
+fbt::vcpu_emulate_cpuid:return / self->rax != NULL / {
+	printf("CPUID query: leaf/subleaf 0x%08x/0x%08x, returns rax = 0x%08x, rbx = 0x%08x, rcx = 0x%08x, rdx = 0x%08x\n",
+		self->leaf,
+	 	self->subleaf,
+		*self->rax,
+		*self->rbx,
+		*self->rcx,
+		*self->rdx
+	);
+	self->rax = 0;
+}
