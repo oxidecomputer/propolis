2a46f5496e865d8a3b3c11bca5b17de203359c9f
Kyle Simpson <kyle@oxide.computer>
2025-08-27 12:45:37 +0100

    Test bumping Tx queue.

--- bin/propolis-standalone/src/main.rs
+++ bin/propolis-standalone/src/main.rs
@@ -1237,7 +1237,7 @@ fn setup_instance(
                     let viona = hw::virtio::PciVirtioViona::new(
                         vnic_name,
                         0x0800.try_into().unwrap(),
-                        0x0100.try_into().unwrap(),
+                        0x0200.try_into().unwrap(),
                         &hdl,
                         viona_params,
                     )?;
