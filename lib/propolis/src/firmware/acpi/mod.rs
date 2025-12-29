8cc2a725ca5a10c8cd519c0fda472a6b99a8e921
Amey Narkhede <ameynarkhede03@gmail.com>
2025-12-29 06:12:25 +0000

    Wire up firmware/acpi module exports
    Export public API for AML generation AmlBuilder, AmlWriter trait, guard
    types (ScopeGuard, DeviceGuard, MethodGuard), EisaId and
    ResourceTemplateBuilder.
    This would enable generating the dynamic bytecode used in tables like
    DSDT.
    
    Signed-off-by: Amey Narkhede <ameynarkhede03@gmail.com>

---
+++ lib/propolis/src/firmware/acpi/mod.rs
@@ -0,0 +1,14 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+//! ACPI table and AML bytecode generation.
+
+pub mod aml;
+pub mod names;
+pub mod opcodes;
+pub mod resources;
+
+pub use aml::{AmlBuilder, AmlWriter, DeviceGuard, MethodGuard, ScopeGuard};
+pub use names::EisaId;
+pub use resources::ResourceTemplateBuilder;
