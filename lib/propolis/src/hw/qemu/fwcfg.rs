77fe185386878a6417785aee2d5ed4e8291337cc
Amey Narkhede <ameynarkhede03@gmail.com>
2025-12-29 02:55:38 +0000

    Add RSDT, XSDT and RSDP tables
    Add builders to generate basic ACPI tables
    RSDP(ACPI 2.0+) that points to XSDT, XSDT with 64-bit table pointers and
    RSDT with 32-bit table pointers that would work with the table-loader
    mechanism in fw_cfg.
    
    These tables are used to describe the ACPI table hierarchy to guest
    firmware. The builders produce raw table data bytes with placeholder
    addresses and checksums that are fixed up by firmware using table-loader
    commands.
    
    Signed-off-by: Amey Narkhede <ameynarkhede03@gmail.com>

--- lib/propolis/src/hw/qemu/fwcfg.rs
+++ lib/propolis/src/hw/qemu/fwcfg.rs
@@ -1476,6 +1476,129 @@ pub mod formats {
     pub const ACPI_TABLES_FWCFG_NAME: &str = "etc/acpi/tables";
     pub const ACPI_RSDP_FWCFG_NAME: &str = "etc/acpi/rsdp";
 
+    pub const ACPI_TABLE_HEADER_SIZE: usize = 36;
+    const ACPI_TABLE_LENGTH_OFFSET: usize = 4;
+
+    #[derive(Copy, Clone, IntoBytes, Immutable)]
+    #[repr(C, packed)]
+    struct AcpiTableHeader {
+        signature: [u8; 4],
+        length: u32,
+        revision: u8,
+        checksum: u8,
+        oem_id: [u8; 6],
+        oem_table_id: [u8; 8],
+        oem_revision: u32,
+        creator_id: [u8; 4],
+        creator_revision: u32,
+    }
+
+    impl AcpiTableHeader {
+        fn new(signature: [u8; 4], revision: u8) -> Self {
+            Self {
+                signature,
+                length: 0,
+                revision,
+                checksum: 0,
+                oem_id: *b"OXIDE\0",
+                oem_table_id: *b"PROPOLIS",
+                oem_revision: 1,
+                creator_id: *b"OXDE",
+                creator_revision: 1,
+            }
+        }
+    }
+
+    #[must_use = "call .finish() to get the table bytes"]
+    pub struct Rsdt {
+        data: Vec<u8>,
+    }
+
+    impl Rsdt {
+        pub fn new() -> Self {
+            let header = AcpiTableHeader::new(*b"RSDT", 1);
+            let mut data = vec![0u8; ACPI_TABLE_HEADER_SIZE];
+            data[..ACPI_TABLE_HEADER_SIZE].copy_from_slice(header.as_bytes());
+            Self { data }
+        }
+
+        pub fn add_entry(&mut self) -> u32 {
+            let offset = self.data.len() as u32;
+            self.data.extend_from_slice(&[0u8; 4]);
+            offset
+        }
+
+        pub fn finish(mut self) -> Vec<u8> {
+            let length = self.data.len() as u32;
+            self.data[ACPI_TABLE_LENGTH_OFFSET..ACPI_TABLE_LENGTH_OFFSET + 4]
+                .copy_from_slice(&length.to_le_bytes());
+            self.data
+        }
+    }
+
+    #[must_use = "call .finish() to get the table bytes"]
+    pub struct Xsdt {
+        data: Vec<u8>,
+    }
+
+    impl Xsdt {
+        pub fn new() -> Self {
+            let header = AcpiTableHeader::new(*b"XSDT", 1);
+            let mut data = vec![0u8; ACPI_TABLE_HEADER_SIZE];
+            data[..ACPI_TABLE_HEADER_SIZE].copy_from_slice(header.as_bytes());
+            Self { data }
+        }
+
+        pub fn add_entry(&mut self) -> u32 {
+            let offset = self.data.len() as u32;
+            self.data.extend_from_slice(&[0u8; 8]);
+            offset
+        }
+
+        pub fn finish(mut self) -> Vec<u8> {
+            let length = self.data.len() as u32;
+            self.data[ACPI_TABLE_LENGTH_OFFSET..ACPI_TABLE_LENGTH_OFFSET + 4]
+                .copy_from_slice(&length.to_le_bytes());
+            self.data
+        }
+    }
+
+    pub const RSDP_SIZE: usize = 36;
+    pub const RSDP_V1_SIZE: usize = 20;
+
+    const RSDP_SIGNATURE_OFFSET: usize = 0;
+    const RSDP_SIGNATURE_LEN: usize = 8;
+    pub const RSDP_CHECKSUM_OFFSET: usize = 8;
+    const RSDP_OEMID_OFFSET: usize = 9;
+    const RSDP_OEMID_LEN: usize = 6;
+    const RSDP_REVISION_OFFSET: usize = 15;
+    const RSDP_LENGTH_OFFSET: usize = 20;
+    pub const RSDP_XSDT_ADDR_OFFSET: usize = 24;
+    pub const RSDP_EXT_CHECKSUM_OFFSET: usize = 32;
+
+    #[must_use = "call .finish() to get the RSDP bytes"]
+    pub struct Rsdp {
+        data: Vec<u8>,
+    }
+
+    impl Rsdp {
+        pub fn new() -> Self {
+            let mut data = vec![0u8; RSDP_SIZE];
+            data[RSDP_SIGNATURE_OFFSET..RSDP_SIGNATURE_OFFSET + RSDP_SIGNATURE_LEN]
+                .copy_from_slice(b"RSD PTR ");
+            data[RSDP_OEMID_OFFSET..RSDP_OEMID_OFFSET + RSDP_OEMID_LEN]
+                .copy_from_slice(b"OXIDE\0");
+            data[RSDP_REVISION_OFFSET] = 2;
+            data[RSDP_LENGTH_OFFSET..RSDP_LENGTH_OFFSET + 4]
+                .copy_from_slice(&(RSDP_SIZE as u32).to_le_bytes());
+            Self { data }
+        }
+
+        pub fn finish(self) -> Vec<u8> {
+            self.data
+        }
+    }
+
     #[cfg(test)]
     mod test_table_loader {
         use super::*;
@@ -1535,6 +1658,23 @@ pub mod formats {
                 loader.add_pointer("a", "b", 0, 3),
                 Err(TableLoaderError::InvalidPointerSize(3))
             ));
+        }
+    }
+
+    #[cfg(test)]
+    mod test_acpi_tables {
+        use super::*;
+
+        #[test]
+        fn basic() {
+            let mut xsdt = Xsdt::new();
+            xsdt.add_entry();
+            let xsdt_data = xsdt.finish();
+            assert_eq!(&xsdt_data[0..4], b"XSDT");
+
+            let rsdp = Rsdp::new();
+            let rsdp_data = rsdp.finish();
+            assert_eq!(&rsdp_data[0..8], b"RSD PTR ");
         }
     }
 }
