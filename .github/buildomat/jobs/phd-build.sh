a978e0d1d6b47821de809800ca2c4233aa36f390
iximeow <iximeow@oxide.computer>
2026-04-17 23:01:39 +0000

    run viona unit tests in CI
    Being an etherstub this isn't a sufficient approach to test a guest can
    reach the actual internet in a VM as one might want in PHD. But it *is*
    enough to set up virtio-nic devices and do some spot checks that the PCI
    device works right.

--- .github/buildomat/jobs/phd-build.sh
+++ .github/buildomat/jobs/phd-build.sh
@@ -30,15 +30,30 @@ rustc --version
 banner prerequisites
 ptime -m ./tools/install_builder_prerequisites.sh -y
 
+# Before we commit to building propolis-server or PHD, there are a handful of
+# tests that are illumos-specific and not currently run in other test jobs. The
+# whole suite doesn't take much time so we'll just run all of them here..
+banner test-propolis
+
+TEST_FEATURES="omicron-build,failure-injection"
+
+# Set up an etherstub to use for VNICs in virtio-nic tests. We might want the
+# tests to run on a real link one day to do actual networking, but we don't need
+# that yet!
+pfexec dladm create-etherstub prop_viona_test0
+VIONA_TEST_NIC=prop_viona_test0 pfexec ptime -m \
+	cargo test --verbose --features "$TEST_FEATURES"
+
 # Build the Propolis server binary with 'dev' profile to enable assertions that
 # should fire during tests.
 banner build-propolis
 
 # Compile propolis-server so that it allows development features to be used even
-# though the `omicron-build` feature is enabled.
-export PHD_BUILD="true"
+# though the `omicron-build` feature is enabled. This should be a relatively
+# small incremental step after building and running tests with the same
+# features, above.
 ptime -m cargo build --verbose -p propolis-server \
-	--features omicron-build,failure-injection
+	--features "$TEST_FEATURES"
 
 # The PHD runner requires unwind-on-panic to catch certain test failures, so
 # build it with the 'dev' profile which is so configured.
