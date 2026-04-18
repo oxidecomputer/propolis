7679fedabb5696817a553f3808eb3457cbdcda4b
iximeow <iximeow@oxide.computer>
2026-04-18 00:36:38 +0000

    maybe this time

--- .github/buildomat/jobs/test-gimlet.sh
+++ .github/buildomat/jobs/test-gimlet.sh
@@ -3,23 +3,46 @@
 #: name = "test-gimlet"
 #: variety = "basic"
 #: target = "lab-2.0-gimlet"
-#: rust_toolchain = "stable"
+#: rust_toolchain = false
+#
+# Why a "lab-2.0-gimlet" target specifically?
 #
 # Propolis has a handful of tests that would like to create and destroy real
 # VMMs. In particular the tests involving virtio-nic set up a viona device
 # which itself requires an actual VMM.
 #
 # This means we have effectively the same constraints as the `phd-run` jobs to
-# run all of the propolis tests. Hence the gimlet target, above.
+# be able to run *all* of the Propolis tests.
 
 set -o errexit
 set -o pipefail
 set -o xtrace
 
+banner prerequisites
+
+# Being on a `gimlet` means that `uname -m` returns "oxide" rather than
+# "i86pc". This in turn means that rustup thinks the CPU is unsupported and
+# bails from setting up toolchains.
+#
+# We know (or, *I* know) that an x86_64-unknown-illumos toolchain will work
+# just fine, so avoid all that and just get one in place by ourselves.
+#
+# Expediency has me requiring that you keep this toolchain version in sync with
+# the one in `rust-toolchain.toml` by hand though, sorry.
+TOOLCHAIN_VER="1.95.0"
+RUST_TAR="rust-"$TOOLCHAIN_VER"-x86_64-unknown-illumos.tar.xz"
+
+wget https://static.rust-lang.org/dist/"$RUST_TAR"
+tar xvf "$RUST_TAR"
+
+# The included install.sh will do; this gets cargo and rustc into
+# /usr/local/bin/, and other bits (like libstd) at the right places.
+bash ./rust-"$TOOLCHAIN_VER"-x86_64-unknown-illumos/install.sh
+
+# ... if everything went right, this should report `TOOLCHAIN_VER`!
 cargo --version
 rustc --version
 
-banner prerequisites
 ptime -m ./tools/install_builder_prerequisites.sh -y
 
 banner test-propolis
