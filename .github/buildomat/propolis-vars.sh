7fe9df78239f168ec70a14b047fcafcccf83f6ff
iximeow <iximeow@oxide.computer>
2026-04-18 02:33:28 +0000

    leftover from earier attempts

--- .github/buildomat/propolis-vars.sh
+++
@@ -1,7 +0,0 @@
-# Define the features used for propolis testing in one place.
-
-# PHD binaries may be built on targets that don't support nested virtualization
-# - cargo build doesn't need it! - so reusing the same feature set in multiple
-# places doesn't let us reuse cached build artifacts, unfortunately. This is
-# just defined in one place to hedge against unexpected feature drift.
-TEST_FEATUERS="omicron-build,failure-injection"
