abf1ce67d2b544928f8774cf60eee7046c9cf2a3
Daniel Levin <daniel.levin@oxidecomputer.com>
2026-04-30 12:29:38 +0000

    more tests

--- crates/dladm/tests/dladm.rs
+++ crates/dladm/tests/dladm.rs
@@ -1,12 +1,13 @@
 #![cfg(target_os = "illumos")]
 
-use dladm::Dladm;
 use dladm::datalink_class_t;
 use dladm::DlAdmOpt;
 use dladm::DlMediaType;
+use dladm::Dladm;
 use dladm::DlpiMediaType;
 
 #[test]
+#[ignore]
 fn empty_link_name() {
     let hnd = Dladm::new().unwrap();
 
@@ -14,6 +15,7 @@ fn empty_link_name() {
 }
 
 #[test]
+#[ignore]
 fn bogus_name() {
     let hnd = Dladm::new().unwrap();
 
@@ -21,6 +23,7 @@ fn bogus_name() {
 }
 
 #[test]
+#[ignore]
 fn query() {
     let hnd = Dladm::new().unwrap();
 
@@ -30,4 +33,24 @@ fn query() {
     assert_eq!(opte0.class, datalink_class_t::DATALINK_CLASS_MISC);
     assert_eq!(opte0.flags, DlAdmOpt::ACTIVE);
     assert_eq!(opte0.media, DlpiMediaType::Known(DlMediaType::Ether));
+}
+
+#[test]
+#[ignore]
+fn query_long_name() {
+    let hnd = Dladm::new().unwrap();
+
+    // To try out:
+    // dladm create-etherstub -t aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa0
+
+    let iface = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa0";
+
+    assert_eq!(iface.as_bytes().len(), 31);
+
+    let link = hnd.describe_link(iface).unwrap();
+
+    assert_eq!(link.mtu.unwrap(), 9000);
+    assert_eq!(link.class, datalink_class_t::DATALINK_CLASS_ETHERSTUB);
+    assert_eq!(link.flags, DlAdmOpt::ACTIVE);
+    assert_eq!(link.media, DlpiMediaType::Known(DlMediaType::Ether));
 }
