7241116da9e379ba7edb58d4cb9b3787559216ee
iximeow <iximeow@oxide.computer>
2026-07-18 01:18:06 +0000

    agh

--- crates/propolis-api-types-versions/src/nvme_write_cache/components/devices.rs
+++ crates/propolis-api-types-versions/src/nvme_write_cache/components/devices.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 use super::super::instance_spec;
 use crate::v1::components::devices::NvmeDisk as V1NvmeDisk;
 use crate::v1::instance_spec::{PciPath, SpecKey};
