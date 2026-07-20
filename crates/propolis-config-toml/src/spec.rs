083c204a02efb9ccfcad993d82dbb921eec4d4fe
iximeow <iximeow@oxide.computer>
2026-07-20 22:25:36 +0000

    imports, fmt

--- crates/propolis-config-toml/src/spec.rs
+++ crates/propolis-config-toml/src/spec.rs
@@ -11,10 +11,11 @@ use std::{
 
 use propolis_client::{
     instance_spec::{
-        Component, Cpuid, CpuidVendor, DlpiNetworkBackend, FileStorageBackend,
-        MigrationFailureInjector, NvmeDisk, P9fs, PciPath, PciPciBridge,
-        SoftNpuP9, SoftNpuPciPort, SoftNpuPort, SpecKey, VirtioDisk,
-        VirtioNetworkBackend, VirtioNic, VirtioSocket,
+        BootOrderEntry, BootSettings, Component, Cpuid, CpuidVendor,
+        DlpiNetworkBackend, FileStorageBackend, MigrationFailureInjector,
+        NvmeDisk, P9fs, PciPath, PciPciBridge, SoftNpuP9, SoftNpuPciPort,
+        SoftNpuPort, SpecKey, VirtioDisk, VirtioNetworkBackend, VirtioNic,
+        VirtioSocket,
     },
     support::nvme_serial_from_str,
 };
@@ -288,18 +289,14 @@ impl TryFrom<&super::Config> for SpecConfig {
         }
 
         if let Some(boot_order) = config.machine_settings.boot_order.as_ref() {
-            let settings = Component::BootSettings(
-                propolis_client::instance_spec::BootSettings {
-                    order: boot_order
-                        .iter()
-                        .map(|key| {
-                            propolis_client::instance_spec::BootOrderEntry {
-                                id: SpecKey::Name(key.to_owned()),
-                            }
-                        })
-                        .collect(),
-                },
-            );
+            let settings = Component::BootSettings(BootSettings {
+                order: boot_order
+                    .iter()
+                    .map(|key| BootOrderEntry {
+                        id: SpecKey::Name(key.to_owned()),
+                    })
+                    .collect(),
+            });
             spec_component_add(
                 &mut spec,
                 SpecKey::Name("boot-settings".to_string()),
